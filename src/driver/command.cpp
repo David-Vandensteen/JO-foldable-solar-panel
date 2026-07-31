848a04280d368e5f68260a7b4b1c28dd056db6a2
David-Vandensteen <dvandensteen@gmail.com>
2026-07-31 11:47:34 +0200

    Integrate DV_DualHoldState for command button actions
    This refactors the `Command` class to utilize the `DV_DualHoldState` component for managing button input. This allows for distinct 'stop' and 'reset' commands to be triggered from a single physical button based on different hold durations, replacing prior custom button state management.

--- src/driver/command.cpp
+++ src/driver/command.cpp
@@ -1,7 +1,26 @@
 #include <Arduino.h>
 #include <ArduinoLog.h>
-#include "setting.h"
+#include <dv_dual_hold_state.h>
 #include "command.h"
+#include "setting.h"
+
+void Command::onStopHold(void *ctx) {
+  if (ctx == nullptr) {
+    return;
+  }
+  Command *self = static_cast<Command *>(ctx);
+  Log.noticeln("Command::stop pressed");
+  self->_state = CommandState::STOPPED;
+}
+
+void Command::onResetHold(void *ctx) {
+  if (ctx == nullptr) {
+    return;
+  }
+  Command *self = static_cast<Command *>(ctx);
+  Log.noticeln("Command::stop reset");
+  self->_state = CommandState::RESET;
+}
 
 Command::Command(SettingBoardPinTrackerCommand *command)
   : _command(command) {}
@@ -10,4 +29,17 @@ void Command::init() {
   Log.traceln("Command::init");
   Log.noticeln("Initializing command");
   pinMode(_command->stop, INPUT_PULLUP);
+  _hold.init(
+    500,
+    Command::onStopHold,
+    1000,
+    Command::onResetHold,
+    this,
+    this
+  );
+}
+
+CommandState Command::update() {
+  _hold.update(digitalRead(_command->stop) == LOW);
+  return _state;
 }
--- src/driver/command.h
+++ src/driver/command.h
@@ -1,47 +1,28 @@
 #ifndef COMMAND_H
 #define COMMAND_H
 #include <Arduino.h>
+#include <dv_dual_hold_state.h>
 #include "setting.h"
+
+enum class CommandState {
+  RUNNING,
+  STOPPED,
+  RESET
+};
 
 class Command {
 public:
   explicit Command(SettingBoardPinTrackerCommand *command);
   void init();
-  #if LOG
-  void log();
-  #endif
+  CommandState update();
 
 private:
+  static void onStopHold(void *ctx);
+  static void onResetHold(void *ctx);
+
+  DV_DualHoldState _hold;
+  CommandState _state = CommandState::RUNNING;
   SettingBoardPinTrackerCommand *_command;
-  struct ButtonState {
-    bool previous = false;
-    bool current = false;
-  };
-  struct State { // TODO: dv_sustain_state
-    ButtonState button;
-    unsigned long pressStart = 0;
-    unsigned long sustain = 1000;
-  };
 };
 
 #endif
-
-/*
-class SustainBool {
-public:
-  SustainBool(unsigned long sustainMs = 1000) : _sustain(sustainMs) {}
-  bool update(bool state) {
-    if (state) {
-      if (_start == 0) _start = millis();
-      if (millis() - _start >= _sustain) return true;
-    } else {
-      _start = 0;
-    }
-    return false;
-  }
-  void setSustain(unsigned long ms) { _sustain = ms; }
-private:
-  unsigned long _start = 0;
-  unsigned long _sustain;
-};
-*/
